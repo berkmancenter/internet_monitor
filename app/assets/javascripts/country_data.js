80591825b04f8386dcc81893ce4cf38218cc02d7
Ryan Westphal <ryan@trippingthebits.com>
2013-04-24 13:43:37 -0400

    check wikipedia for (country) on disambiguation page

--- app/assets/javascripts/country_data.js
+++ app/assets/javascripts/country_data.js
@@ -1,4 +1,8 @@
 $(function() {
+  $(".country-data select").change( function( ) {
+    window.location.href = $(this).val();
+  } );
+
   var $countryWikipedia = $(".country-wikipedia");
 
   if ( $countryWikipedia.length > 0 ) {
@@ -8,15 +12,24 @@ $(function() {
     
 
     $.ajax({
-        url: "http://en.wikipedia.org/w/api.php?action=mobileview&sections=0&format=json&noheadings&page=" + countryName,
-        dataType: "jsonp",
-        success: function(result) {
-          $("<div>").html(result.mobileview.sections[0].text).find(".infobox").appendTo($countryWikipedia).find("a").attr("href", "javascript:void(0);");
-        },
-        error: function (xhr) {
-          // ignore
-          //alert(xhr.status + " " + xhr.statusText);
+      url: "http://en.wikipedia.org/w/api.php?action=mobileview&sections=0&format=json&noheadings&page=" + countryName,
+      dataType: "jsonp",
+      success: function(result) {
+        var infobox = $("<div>").html(result.mobileview.sections[0].text).find(".infobox");
+        
+        if ( infobox.length > 0 ) {
+          infobox.appendTo($countryWikipedia).find("a").attr("href", "javascript:void(0);");
+        } else {
+          // try (country)
+          $.ajax({
+            url: "http://en.wikipedia.org/w/api.php?action=mobileview&sections=0&format=json&noheadings&page=" + countryName + "_(country)",
+            dataType: "jsonp",
+            success: function(result) {
+              $("<div>").html(result.mobileview.sections[0].text).find(".infobox").appendTo($countryWikipedia).find("a").attr("href", "javascript:void(0);");
+            }
+          });
         }
+      }
     });
   }
 });
