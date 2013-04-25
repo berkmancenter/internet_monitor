6a871dc8b4101276b9fe46db34ea969aa7e224db
Ryan Westphal <ryan@trippingthebits.com>
2013-04-25 15:49:52 -0400

    give cms site a name

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -15,7 +15,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  # config.site_name = "Company Name"
+  config.site_name = "Emily Dickinson Archive"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
