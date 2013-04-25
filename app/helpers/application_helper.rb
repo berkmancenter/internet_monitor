d1e33865c65c2b0714f5c5a715cea6de5cb57a99
Ryan Westphal <ryan@trippingthebits.com>
2013-04-25 17:52:41 -0400

    better titles for refinery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  # return supplied title, base refinery controller name (e.g., blog from /refinery/blog/posts)
+  def get_title( title, params )
+    if ( title != "" )
+      title.downcase
+    else
+      controller = params[:controller]
+
+      if ( controller.match( /^refinery/ ) != nil )
+        parts = controller.split( "/" )
+
+        if ( parts[ 1 ] == "pages" )
+          params[ :path ]
+        else
+          parts[ 1 ]
+        end
+      else
+        controller
+      end
+    end
+  end
 end
