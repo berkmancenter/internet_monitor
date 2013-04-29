6f992365d6cafd4c2b2a2f5fe704558f85e4859e
Ryan Westphal <ryan@trippingthebits.com>
2013-04-29 13:17:45 -0400

    This is not EDA.

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -15,7 +15,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "Emily Dickinson Archive"
+  config.site_name = "Internet Monitor"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
