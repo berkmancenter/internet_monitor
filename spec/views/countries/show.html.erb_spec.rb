f2e9748c31f0e572edb0852ee831e3ea4eb85fa1
Ryan Westphal <ryan@trippingthebits.com>
2013-09-06 17:34:18 -0400

    clean up country view a bit; define more colors;

---
+++ spec/views/countries/show.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe ( 'countries/show' ) {
+  subject { rendered }
+
+  context ( 'normal country' ) {
+    let ( :country ) { Country.find_by_iso3_code( 'IRN' ) }
+
+    before {
+      assign( :country, country )
+      render
+    }
+
+    it {
+      should have_css '.view h1'
+    }
+
+    it ( 'h1 no longer has score' ) {
+      should have_css 'h1', text: country.name
+      should_not have_css 'h1', text: country.score
+    }
+
+    it ( 'h1 & .sub-content are siblings' ) {
+      should have_css 'h1~.sub-content'
+    }
+
+    it ( 'sidebar now in sub-content' ) {
+      should have_css '.sub-content .country-data'
+    }
+  }
+}
