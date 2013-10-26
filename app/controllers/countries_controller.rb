ed6af23778aed6f905d72d2c23d25586a8a7c483
Ryan Westphal <ryan@trippingthebits.com>
2013-10-25 21:25:36 -0400

    reformat

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,30 +1,30 @@
 class CountriesController < ApplicationController
-    def index
-        @scored_countries = Country.with_enough_data.desc_score
-        @unscored_countries = Country.without_enough_data
-        respond_to do |format|
-            format.html
-            format.any(:xml, :json)
-        end
+  def index
+    @scored_countries = Country.with_enough_data.desc_score
+    @unscored_countries = Country.without_enough_data
+    respond_to do |format|
+      format.html
+      format.any(:xml, :json)
     end
+  end
 
-    def show
-        @map_countries = Country.with_enough_data.select( 'id,iso3_code,score' )
-        @country = Country.find(params[:id])
-        if params[:category_slug]
-            @category = Category.find(params[:category_slug])
-            render "country_categories/show"
-        else
-            respond_to do |format|
-                format.html
-                format.any(:xml, :json)
-            end
-        end
+  def show
+    @map_countries = Country.with_enough_data.select( 'id,iso3_code,score' )
+    @country = Country.find(params[:id])
+    if params[:category_slug]
+      @category = Category.find(params[:category_slug])
+      render "country_categories/show"
+    else
+      respond_to do |format|
+        format.html
+        format.any(:xml, :json)
+      end
     end
+  end
 
-    def map
-        @map_countries = Country.with_enough_data.select( 'id,iso3_code,score' )
-        @scored_countries = Country.order( 'score desc' ).with_enough_data
-        @unscored_countries = Country.without_enough_data
-    end
+  def map
+    @map_countries = Country.with_enough_data.select( 'id,iso3_code,score' )
+    @scored_countries = Country.order( 'score desc' ).with_enough_data
+    @unscored_countries = Country.without_enough_data
+  end
 end
