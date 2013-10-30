0bab86b66533edffabb83c2147288db74557a94b
Ryan Westphal <ryan@trippingthebits.com>
2013-10-30 18:04:18 -0400

    #6191 link to research & data pages in CMS

--- spec/requests/layouts_spec.rb
+++ spec/requests/layouts_spec.rb
@@ -11,6 +11,8 @@ describe 'layout requests', :js => true do
       should have_css 'header nav'
 
       should have_css "header a[href*='#{refinery::marketable_page_path('about')}']"
+      should have_css "header a[href*='#{refinery::marketable_page_path('research')}']"
+      should have_css "header a[href*='#{refinery::marketable_page_path('data')}']"
       should have_css "header a[href*='#{map_path}']"
       should have_css "header a[href*='#{countries_path}']"
       should have_css "header a[href*='#{refinery::blog_root_path}']"
