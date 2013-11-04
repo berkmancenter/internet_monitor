bba6863ba9262c458b9233dbc50cebd7f914ea47
Ryan Westphal <ryan@trippingthebits.com>
2013-11-04 14:45:43 -0500

    nicer display for country index page tho users won't see it

---
+++ spec/views/countries/index.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe ( 'countries/index' ) {
+  subject { rendered }
+
+  context ( 'default view' ) {
+    before {
+      assign( :scored_countries, Country.with_enough_data )
+      assign( :unscored_countries, Country.without_enough_data )
+    
+      render
+    }
+
+    it {
+      should have_css 'h2', text: 'Countries', exact: true
+      should have_css 'ol.scored-countries' #ordered
+
+      should have_css 'h2', text: 'Countries Without Scores'
+      should have_css 'ul.unscored-countries' #unordered
+    }
+
+    it {
+      should have_css 'ol .score-pill'
+      should have_css 'ul .score-pill'
+    }
+  }
+}
