4419f2b384ab66ef6f9e8e081b028b78b07d9287
Ryan Westphal <ryan@trippingthebits.com>
2014-01-27 15:58:44 -0500

    reformat

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,10 +1,10 @@
 class Indicator < Datum
-    delegate :min, :max, :to => :source
-    alias_attribute :percent, :value
+  delegate :min, :max, :to => :source
+  alias_attribute :percent, :value
 
-    def calc_percent
-        percent = (original_value-min) / (max-min)
-        percent = 1 - percent if source.default_weight < 0
-        self.percent = percent
-    end
+  def calc_percent
+    percent = (original_value-min) / (max-min)
+    percent = 1 - percent if source.default_weight < 0
+    self.percent = percent
+  end
 end
