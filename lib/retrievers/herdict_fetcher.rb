8e0309f66afb449092279dcbcb6f5dd354e3a37d
Ryan Westphal <ryan@trippingthebits.com>
2014-01-31 13:49:41 -0500

    steps toward new fetcher

--- lib/retrievers/herdict_fetcher.rb
+++ lib/retrievers/herdict_fetcher.rb
@@ -1,33 +1,21 @@
 class HerdictFetcher
-  def top_sites_by_country(country_code)
-    start_date = Date.today - 365
-    start_date = start_date.strftime('%m/%d/%Y')
-    response = response(country_code, start_date)
-    sites = parse_response(response)
-    sites.sort_by!{|s| s[1] }.reverse!.delete_if{|s| s[1] == 0}.map{|s| s[0]}
+  def top_sites_by_country( country_code, year )
+    response country_code, "1/1/#{year}", "12/31/#{year}"
   end
 
-  def parse_response(response)
-    sites = []
-    doc = Nokogiri::HTML(response)
-    doc.css('.topitems-list li').each do |li|
-      sites << [li.at('a').text, (li > '.inaccessible').text.to_i]
-    end
-    sites
-  end
-
-  def response(country_code, start_date)
-    HTTParty.get("http://www.herdict.org/explore/module/topsitescategory?631=&fc=#{URI.encode(country_code)}&fsd=#{URI.encode(start_date)}").body
+  def response( country_code, start_date, end_date )
+    HTTParty.get("http://www.herdict.org/explore/module/topsitescategory?fc=#{country_code}&fsd=#{URI.encode(start_date)}&fed=#{URI.encode( end_date )}").body
   end
 
   def data(options = {})
     data = []
-    Country.all.each do |country|
-      sites = top_sites_by_country(country.iso_code)
-      u = UrlList.new(:start_date => Date.today - 365, :value => sites)
-      u.country = country
+    Country.all.each { |country|
+      sites = top_sites_by_country(country.iso_code, 2013)
+      d = HtmlBlock.new :start_date => Date.today - 365, :value => sites)
+      d.country = country
       data << u
-    end
+    }
+
     data
   end
 end
