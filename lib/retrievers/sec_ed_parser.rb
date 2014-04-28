53dc387358a949b2eac397927e61cb42261255dc
Ryan Westphal <ryan@trippingthebits.com>
2014-04-28 13:52:38 -0400

    #7505 - add secondary ed indicator

--- lib/retrievers/sec_ed_parser.rb
+++ lib/retrievers/sec_ed_parser.rb
@@ -1,17 +1,20 @@
-class LitParser
+class SecEdParser
   require 'csv'
 
   def data(options = {})
+    sheetname = options[:sheetname]
+    column = options[:column]
     filename = options[:filename]
     data = []
     csv = CSV.open filename, { headers: true }
     csv.each do |row|
-      country = Country.find_by_name row['Country or Area']
+      country = Country.find_by_iso3_code row['Country Code']
       next unless country
+      next if row[column] == '..'
 
       i = Indicator.new( {
-        start_date: Date.new(2011, 1, 1),
-        original_value: row[ 'Value' ].to_f
+        start_date: Date.new(2010, 1, 1),
+        original_value: row[ column ].to_f
       } )
       i.country = country
       data << i
