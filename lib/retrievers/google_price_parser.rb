3960e997d90911e4681a5ee52b39004bf084faa6
Ryan Westphal <ryan@trippingthebits.com>
2014-06-06 10:34:33 -0400

    #8219 - add aggregate index source

--- lib/retrievers/google_price_parser.rb
+++ lib/retrievers/google_price_parser.rb
@@ -6,7 +6,7 @@ class GooglePriceParser
     filename = options[:filename]
     column = options[:column]
     normalized_column = options[:normalized_column]
-    multiplier = options[:multiplier]
+    multiplier = options[:multiplier] || 1.0
     data = []
     csv = CSV.open(filename, { :headers => true })
     csv.each do |row|
@@ -15,7 +15,7 @@ class GooglePriceParser
 
       start_date = Date.new(years.to_i, 1, 1)
       datum = row[column].to_f * multiplier unless row[column].nil?
-      normalized = row[normalized_column].to_f
+      normalized = row[normalized_column].to_f unless row[normalized_column].nil?
       unless datum.nil?
         i = Indicator.new(:start_date => start_date, :original_value => datum, :value => normalized)
         i.country = country
