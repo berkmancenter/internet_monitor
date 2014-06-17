c5f081fe171ebb6ef0341b25a139d2e9fe47bfc1
Ryan Westphal <ryan@trippingthebits.com>
2014-06-17 16:28:29 -0400

    #8576 - new lit rate data

--- lib/retrievers/lit_parser.rb
+++ lib/retrievers/lit_parser.rb
@@ -3,15 +3,16 @@ class LitParser
 
   def data(options = {})
     filename = options[:filename]
+    multiplier = options[:multiplier] || 1.0
     data = []
     csv = CSV.open filename, { headers: true }
     csv.each do |row|
-      country = Country.find_by_name row['Country or Area']
+      country = Country.find_by_name row['Country']
       next unless country
 
       i = Indicator.new( {
-        start_date: Date.new(2011, 1, 1),
-        original_value: row[ 'Value' ].to_f
+        start_date: Date.new(2014, 6, 12),
+        original_value: row[ 'Literacy rate' ].to_f * multiplier
       } )
       i.country = country
       data << i
