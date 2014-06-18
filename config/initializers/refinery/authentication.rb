fe38251617762afb985dd116f4d8c342c653192d
Ryan Westphal <ryan@trippingthebits.com>
2014-06-18 15:31:54 -0400

    allow refinery admin to assign roles

--- config/initializers/refinery/authentication.rb
+++ config/initializers/refinery/authentication.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 Refinery::Authentication.configure do |config|
   # Configure whether to allow superuser to assign roles
-  # config.superuser_can_assign_roles = false
+  config.superuser_can_assign_roles = true
 end
