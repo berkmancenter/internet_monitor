0fd3ecdb4c3b52dfe5b2d99cea8ccd463b99a2b1
Ryan Westphal <ryan@trippingthebits.com>
2014-06-24 13:00:38 -0400

    #8813 - comment up algorithm in ruby

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -3,12 +3,30 @@ class Indicator < Datum
   alias_attribute :percent, :value
 
   def self.weighted_score( indis )
+    # calculate the score for a group of indicators
+    # called by each country with the most recent indicators affecting score
+
+    # reject all empty indicators
     indis.reject!{|i| i.percent.nil? || i.percent.nan?}
+
+    # run through all the indicators and produce a sum
     sum = indis.reduce(0.0) do |sum, i|
+      # with each indicator...
+
+      # multiply by the default weight
+      # will also turn value negative if it's a backwards indicator
       increment = i.percent * i.source.default_weight
+
+      # if it's a backwards indicator, add one
+      # (since backwards indicator values are now negative by this point)
       increment += 1.0 if i.source.default_weight < 0
+
+      # add this value to the growing sum
       sum + increment
     end
+
+    # the final sum is the average of indicator values calculated above
+    # times the highest score possible, 10.0
     sum / indis.count.to_f * Rails.application.config.imon['max_score']
   end
 
