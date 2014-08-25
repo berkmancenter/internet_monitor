494ff7a93c621719a967839e13aff374b38cbdc9
Ryan Westphal <ryan@trippingthebits.com>
2014-08-25 15:44:13 -0400

    reformat

--- app/controllers/datum_sources_controller.rb
+++ app/controllers/datum_sources_controller.rb
@@ -1,13 +1,13 @@
 class DatumSourcesController < ApplicationController
-    def index
-        if params[:type]
-            @datum_sources = DatumSource.where(:datum_type => params[:type])
-        else
-            @datum_sources = DatumSource.all
-        end
-        respond_to do |format|
-            format.html
-            format.any(:json, :xml)
-        end
+  def index
+    if params[:type]
+      @datum_sources = DatumSource.where(:datum_type => params[:type])
+    else
+      @datum_sources = DatumSource.all
     end
+    respond_to do |format|
+      format.html
+      format.any(:json, :xml)
+    end
+  end
 end
