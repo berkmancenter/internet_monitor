d4843cc742efe1d0cad17b3aa5be2b043b66157e
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2016-06-27 15:14:27 -0400

    #13475 - update all countries

--- app/controllers/v2/countries_controller.rb
+++ app/controllers/v2/countries_controller.rb
@@ -1,24 +1,18 @@
 class V2::CountriesController < ApplicationController
   def index
-    @countries = Country.with_enough_data.desc_score
-
-    indicators = []
-    @countries.each { |c|
-      indicators |= c.indicators.in_current_index
-    }
+    @countries = Country.all
     render json: {
       data: @countries.map { |c|
-        json = c.as_jsonapi
+        json = c.as_jsonapi_v2
         json[ :links ][ :self ] = v2_country_url( c )
 
         json
-      },
-      included: indicators.map( &:as_jsonapi )
+      }
     }
   end
 
   def show
-    @country = Country.with_enough_data.find(params[:id])
+    @country = Country.find(params[:id])
     json = @country.as_jsonapi_v2
     json[ :links ][ :self ] = v2_country_url( @country )
     render json: {
