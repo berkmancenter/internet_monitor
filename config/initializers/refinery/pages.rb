1c58d8e400aeb832398d31ac44f42b2b470d3785
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-01-23 13:47:54 -0500

    #13964 - report layout

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -39,11 +39,11 @@ Refinery::Pages.configure do |config|
   # Set this to true to fully expand the page hierarchy in the admin
   # config.auto_expand_admin_tree = true
 
-  config.layout_template_whitelist = ["application"]
+  config.layout_template_whitelist = ["application", "report"]
 
   # config.view_template_whitelist = ["home", "show"]
 
-  config.use_layout_templates = false
+  config.use_layout_templates = true
 
   config.use_view_templates = false
 
