b7c35972849426ead38eaa08ec33afaa265c4308
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-09-01 12:58:45 -0400

    #14370 - expand references section on ref click

--- app/assets/javascripts/sitewide/refinery_pages.js
+++ app/assets/javascripts/sitewide/refinery_pages.js
@@ -1,7 +1,18 @@
-$( '.reference' ).each( function ( i ) {
-  $( this ).find( 'a' ).attr( 'href', '#cite-note-' + ( i + 1 ) ).text( '[' + ( i + 1 ) + ']' );
-} );
+(function() {
+  $( '.reference' ).each( function ( i ) {
+    $( this ).find( 'a' ).attr( 'href', '#cite-note-' + ( i + 1 ) ).text( '[' + ( i + 1 ) + ']' );
+  } );
 
-$( '.references > li' ).each( function ( i ) {
-  $( this ).attr( 'id', 'cite-note-' + ( i + 1 ) );
-} );
+  $( '.references > li' ).each( function ( i ) {
+    $( this ).attr( 'id', 'cite-note-' + ( i + 1 ) );
+  } );
+
+
+  $( '.reference a' ).click( function() {
+    var section = $( '.references' ).prev( '.expandable' );
+    var show = !section.hasClass( 'expanded' );
+    if ( show ) {
+      section.click();
+    }
+  } );
+} ());
