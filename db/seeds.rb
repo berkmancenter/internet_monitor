ef2a3cd821e3d42d6be13130f32af6c839be5971
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-10-02 20:54:11 -0400

    fix db seeds, it's old

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,63 +8,10 @@
 require 'csv'
 require 'roo'
 
-# Create categories
-categories = ['Access', 'Control', 'Activity'].map{|n| Category.find_or_create_by_name(n)}
-
-# groups
-Group.find_or_create_by_admin_name_and_public_name 'adoption', 'Adoption'
-Group.find_or_create_by_admin_name_and_public_name 'speed', 'Speed and Quality'
-Group.find_or_create_by_admin_name_and_public_name 'price', 'Price'
-Group.find_or_create_by_admin_name_and_public_name 'human', 'Literacy and Gender Equality'
-
-Group.find_or_create_by_admin_name_and_public_name 'control', 'Internet Control'
-Group.find_or_create_by_admin_name_and_public_name 'filtering', 'Internet Filtering'
-Group.find_or_create_by_admin_name_and_public_name 'filtering_mo', 'Internet Filtering MO'
-
-# Create Lanuages
-CSV.open(Rails.root.join('db','iso-639-3_20130123.tab'), {:col_sep => "\t", :headers => true}).each do |line|
-    Language.create(:name => line['Ref_Name'], :iso_code => (line['Part1'] || line['Id']))
-end
-
 # Create countries and connect to languages
 CSV.open(Rails.root.join('db', 'countryInfo.txt'), {:headers => true, :col_sep => "\t"}).each do |line|
-    country = Country.find_or_initialize_by_iso_code(:iso_code => line['ISO'], :name => line['Country'], :iso3_code => line['ISO3'])
-    country.categories = categories
-    unless line['Languages'].nil?
-        line['Languages'].split(',').each do |language|
-            country.languages << Language.find_by_iso_code(language.split('-')[0])
-        end
-    end
-    country.save!
+  country = Country.create iso_code: line['ISO'], name: line['Country'], iso3_code: line['ISO3']
 end
-
-# Create regions
-CSV.open(Rails.root.join('db', 'regions.csv'), {:headers => true}).each do |line|
-  region = Country.find_or_initialize_by_iso3_code(:iso3_code => line['cc3'], :name => line['region'])
-  region.categories = categories
-  region.save!
-end
-
-# Import country bboxen
-
-import_country_bboxes 'db/data_files/country_bbox.json'
-
-# Import all the data
-sources = Roo::Excelx.new Rails.root.join('db', 'sources.xlsx').to_s
-
-CSV.parse( sources.sheet( sources.default_sheet ).to_csv, { :headers => true } ).each_with_index do |row, i|
-    next unless row['Include in Internet Monitor?'] == 'y'
-    Retriever.retrieve!(i + 1)
-end
-
-puts 'Country.count_indicators'
-Country.count_indicators!
-
-puts 'DatumSource.recalc_min_max_and_values!'
-DatumSource.recalc_min_max_and_values!
-
-puts 'Country.calculate_scores_and_rank!'
-Country.calculate_scores_and_rank!
 
 # Added by Refinery CMS Pages extension
 Refinery::Pages::Engine.load_seed
