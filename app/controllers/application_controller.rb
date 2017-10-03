ab7f2c118dade7cb6aae0f67dc32a7e4d3db6c46
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-10-02 20:54:30 -0400

    add bulletins block link to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :load_groups
   
   rescue_from ActionController::RoutingError, with: :render_not_found
-
-  def load_groups
-    access = Category.find_by_slug 'access'
-    @groups = DatumSource.where( { category_id: access.id } ).map { |ds| ds.group }.uniq
-  end
 
   def not_found
     raise ActionController::RoutingError.new( 'Not Found' )
